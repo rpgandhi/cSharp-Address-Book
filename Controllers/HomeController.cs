f601010ba05a9d6d1ce09202b5baf0289fd615d5
Rakhee Gandhi <rpgandhi@gmail.com>
2017-10-15 10:57:53 -0700

    Create initial files and build.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AddressBook.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Hello() { return "Hello friend!"; }
+    }
+}
