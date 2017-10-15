c3c380217525a8e69fb112b5bbea6b63c567d8f0
Rakhee Gandhi <rpgandhi@gmail.com>
2017-10-15 15:59:18 -0700

    Add Controllers, Models, Contact object, and link pages to root.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
+using AddressBook.Models;
 
 namespace AddressBook.Controllers
 {
     public class HomeController : Controller
     {
-        public string Hello() { return "Hello friend!"; }
+        [Produces("text/html")]
+        [Route("/")]
+        public ActionResult Index()
+        {
+          Contact myContact = new Contact();
+          myContact.SetName(Request.Query["name"]);
+          myContact.SetAddress(Request.Query["address"]);
+          myContact.SetPhone(Request.Query["phone"]);
+          return View(myContact);
+        }
+
+        [Produces("text/html")]
+        [Route("/NewContact")]
+        public ActionResult NewContact()
+        {
+          return View();
+        }
+
+        [Produces("text/html")]
+        [Route("/AddConfirm")]
+        public ActionResult AddConfirm()
+        {
+          return View();
+        }
+
+        [Produces("text/html")]
+        [Route("/DeleteConfirm")]
+        public ActionResult DeleteConfirm()
+        {
+          return View();
+        }
     }
 }
---
+++ Models/Contact.cs
@@ -0,0 +1,38 @@
+// using System;
+// using System.Collections.Generic;
+
+namespace AddressBook.Models
+{
+  public class Contact
+  {
+    private string _name;
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+
+    private string _address;
+    public string GetAddress()
+    {
+      return _address;
+    }
+    public void SetAddress(string newAddress)
+    {
+      _address = newAddress;
+    }
+
+    private string _phone;
+    public string GetPhone()
+    {
+      return _phone;
+    }
+    public void SetPhone(string newPhone)
+    {
+      _phone = newPhone;
+    }
+  }
+}
